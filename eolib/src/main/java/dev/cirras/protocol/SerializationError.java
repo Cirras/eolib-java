22b5c81f2fa9eea89cbd8d2e380f3df10807ce0b
Jonah Jeleniewski <cirrasbu@hotmail.com>
2022-12-22 21:07:30 +1100

    Document the `SerializationError` exception

--- eolib/src/main/java/dev/cirras/protocol/SerializationError.java
+++ eolib/src/main/java/dev/cirras/protocol/SerializationError.java
@@ -1,6 +1,12 @@
 package dev.cirras.protocol;
 
+/** This exception reports errors in serializing a protocol data structure. */
 public final class SerializationError extends RuntimeException {
+  /**
+   * Constructs a {@code SerializationError} with the specified detail message.
+   *
+   * @param message the detail message
+   */
   public SerializationError(String message) {
     super(message);
   }
