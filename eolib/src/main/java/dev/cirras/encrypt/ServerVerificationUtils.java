ccddb82e5999a1db2adcd437dba1fd188fada2bc
Jonah Jeleniewski <cirrasbu@hotmail.com>
2022-12-27 00:37:32 +1100

    Implement `ServerVerificationUtils`

---
+++ eolib/src/main/java/dev/cirras/encrypt/ServerVerificationUtils.java
@@ -0,0 +1,32 @@
+package dev.cirras.encrypt;
+
+/** A utility class for verifying that the game server is genuine. */
+public class ServerVerificationUtils {
+  private ServerVerificationUtils() {
+    // utility class
+  }
+
+  /**
+   * This hash function is how the game client checks that it's communicating with a genuine server
+   * during connection initialization.
+   *
+   * <ul>
+   *   <li>The client sends an integer value to the server in the INIT_INIT client packet, where it
+   *       is referred to as the {@code challenge} value.
+   *   <li>The server hashes the value and sends it back in the INIT_INIT server packet.
+   *   <li>The client hashes the value and compares it to the value sent by the server.
+   *   <li>If the hashes don't match, the client drops the connection.
+   * </ul>
+   *
+   * @param challenge the challenge value sent by the client
+   * @return the hashed challenge value
+   */
+  public static int serverVerificationHash(int challenge) {
+    ++challenge;
+    return 110905
+        + (challenge % 9 + 1)
+            * Integer.remainderUnsigned(11092004 - challenge, (challenge % 11 + 1) * 119)
+            * 119
+        + challenge % 2004;
+  }
+}
---
+++ eolib/src/test/java/dev/cirras/encrypt/ServerVerificationUtilsTest.java
@@ -0,0 +1,34 @@
+package dev.cirras.encrypt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import dev.cirras.data.EONumericLimits;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+final class ServerVerificationUtilsTest {
+  private static class ServerVerificationHashArgumentsProvider implements ArgumentsProvider {
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+      return Stream.of(
+          Arguments.of(0, 114000),
+          Arguments.of(1, 115191),
+          Arguments.of(2, 229432),
+          Arguments.of(5, 613210),
+          Arguments.of(12345, 266403),
+          Arguments.of(100_000, 145554),
+          Arguments.of(EONumericLimits.THREE_MAX - 1, 138566));
+    }
+  }
+
+  @ParameterizedTest(name = "{0} should hash to {1}")
+  @ArgumentsSource(ServerVerificationHashArgumentsProvider.class)
+  void testServerVerificationHash(int challenge, int expectedHash) {
+    int hash = ServerVerificationUtils.serverVerificationHash(challenge);
+    assertThat(hash).isEqualTo(expectedHash);
+  }
+}
